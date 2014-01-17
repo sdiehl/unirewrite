fc2176a888d747582e815131afa1c018389f4572
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-01-16 22:59:43 -0600

    rewrite toplevel loop

--- src/Strategy.hs
+++ src/Strategy.hs
@@ -1,42 +1,135 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 module Strategy (
+  succeed,
+  failure,
+  seqn,
+
+  until,
+  while,
+
+  left,
+  right,
+
+  apply,
+  reduce,
+  compose,
+  composes,
 ) where
 
+import Prelude hiding (repeat, fail, sequence, until)
+
 import Data.Data
+import Data.List
+import Data.Maybe
 
 import Control.Monad
 import Control.Applicative
 
 import Data.Generics.Uniplate.Data
 
+-- | /Identity/
+--
+-- @
+-- ϵ
+-- @
+--
+-- do nothing succesfully
 succeed :: a -> Maybe a
 succeed = Just
 
-fail :: a -> Maybe a
-fail = const Nothing
+-- | /Failure/
+--
+-- @
+-- δ
+-- @
+--
+-- Do nothing unsuccesfully
+failure :: a -> Maybe a
+failure = const Nothing
 
--- r1 > r2
--- Apply r1, if it succeeds then apply r2
-composeM :: Monad m => [a -> m a] -> a -> m a
-composeM = foldr (>=>) return
+-- | /Sequence/
+--
+-- @
+-- a ; b
+-- @
+--
+-- First do a, then do that b
+seqn :: MonadPlus m => (t -> m a) -> (t -> m a) -> t -> m a
+seqn s t = \x -> s x `mplus` t x
 
+-- | /Non-deterministic choice/
+--
+-- Left biased
+--
+-- @
+-- a + b
+-- @
+--
+-- Do a, if a fails then do b.
 left :: (a -> Maybe a) -> (a -> Maybe a) -> (a -> Maybe a)
 left = liftA2 (<|>)
 
+-- | /Non-deterministic choice/
+--
+-- Right biased
+--
+-- @
+-- b + a
+-- @
+--
+-- Do b, if b fails then do a.
 right :: (a -> Maybe a) -> (a -> Maybe a) -> (a -> Maybe a)
 right = flip $ liftA2 (<|>)
 
-lefts :: Data a => [a -> Maybe a] -> a -> Maybe a
-lefts = foldl1 left
+-- | /Reduce/
+--
+-- @
+-- ◯ a
+-- @
+--
+-- Apply rule until it no longer applies.
+reduce :: Data on => (on -> Maybe on) -> on -> on
+reduce = rewrite
+
+-- | /Apply/
+--
+-- @
+-- ☐ a
+-- @
+--
+-- Apply rule once to all subexpressions in bottom-up.
+
+apply :: Data on => (on -> Maybe on) -> on -> on
+apply f = transform g
+  where g x = maybe x id (f x)
 
 try ::  (a -> Maybe a) -> a -> Maybe a
 try f = f `left` succeed
 
-rights :: [a -> Maybe a] -> a -> Maybe a
-rights = foldl1 right
+-- | Repeat while a predicate holds.
 
--- Repeatedly apply (r1 | r2 | ... rn) until rn fails.
-switch ::  Data on => [on -> Maybe on] -> on -> on
-switch = rewrite . lefts
+while :: Data a => (a -> Bool) -> (a -> Maybe a) -> a -> Maybe a
+while p f x = if p x
+  then while p f (apply f x)
+  else Just x
 
-series :: Data a => [a -> Maybe a] -> a -> Maybe a
-series = lefts . map ((Just .) . rewrite)
+-- | Repeat until a predicate holds.
+
+until :: Data a => (a -> Bool) -> (a -> Maybe a) -> a -> Maybe a
+until p f x = if not (p x)
+  then while p f (apply f x)
+  else Just x
+
+compose :: (a -> Maybe a) -> (a -> Maybe a) -> a -> Maybe a
+compose f g a = f a `mplus` g a
+
+composes :: MonadPlus m => [t -> m a] -> t -> m a
+composes fs x = msum [f x | f <- fs]
+
+-- unbounded fixpoint
+fixp :: (a -> Maybe a) -> a -> a
+fixp f = last . fixpl f
+
+fixpl :: (a -> Maybe a) -> a -> [a]
+fixpl f a = a : maybe [] (fixpl f) (f a)
--- src/Unirewrite.hs
+++ src/Unirewrite.hs
@@ -1,70 +1,96 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Unirewrite (
+  evaluatorLoop,
+  EvalState,
+  Trans
 ) where
 
+import Control.Monad.RWS
+import Control.Monad.Reader as R
+
 import Data.Data
-
-import Control.Monad
-import Control.Monad.State
-
 import Data.Generics.Uniplate.Data
 
 -------------------------------------------------------------------------------
--- Composite Uniplate
--------------------------------------------------------------------------------
-
-composeM :: MonadPlus m => [t -> m a] -> t -> m a
-composeM ts x = msum [t x | t <- ts]
-
-{-composeM :: Monad m => [a -> m a] -> a -> m a-}
-{-composeM = foldr (>=>) return-}
-
-rewrites :: Data on => [on -> Maybe on] -> on -> on
-rewrites = rewrite . composeM
-
-{-rewritesM = rewriteM . composeM-}
-
--------------------------------------------------------------------------------
--- Rewrite Termination
+-- Evaluation State
 -------------------------------------------------------------------------------
 
 data Status = Success | Failed deriving (Show)
-{-data Status a = Success a | Failed deriving (Show)-}
 
-istate :: (Status, Integer)
-istate = (Success, 0)
+data EvalState = EvalState
+    { depth :: Integer
+    , iter  :: Integer
+    , status :: Status
+    } deriving (Show)
 
--- |Perform a rewrite a maximum of `u` times before failing. Uses
--- State to track success of failure of rewrite.
-limit :: Show b => Integer -> (b -> Maybe b) -> b -> State (Status, Integer) (Maybe b)
-limit u f x = do
-  (_, i) <- get
-  if i < u then do
-    put (Success, i + 1)
-    let res = f x
-    return res
-    {-return $ trace ("Iter" ++ (show res)) res-}
+defaultEvalState :: EvalState
+defaultEvalState = EvalState 0 0 Success
+
+incDepth :: Eval c a ()
+incDepth = modify $ \s -> s { depth = (depth s) + 1 }
+
+decDepth :: Eval c a ()
+decDepth = modify $ \s -> s { depth = (depth s) - 1 }
+
+incIter :: Eval c a ()
+incIter = modify $ \s -> s { iter = (iter s) + 1 }
+
+decIter :: Eval c a ()
+decIter = modify $ \s -> s { iter = (iter s) - 1 }
+
+-------------------------------------------------------------------------------
+-- Eval Loop
+-------------------------------------------------------------------------------
+
+type Trans t a = a -> Reader t (Maybe a)
+
+type Eval c a r = RWS c     -- ^Evaluation context (properties, definitions)
+                  [a]       -- ^Steps
+                  EvalState -- ^Evaluation state
+                  r         -- ^Result
+
+limitReached :: Eval c a Bool
+limitReached = do
+  i <- gets depth
+  j <- gets iter
+  return $ i > 1000 || j > 1000
+
+runTrans :: c -> (a -> R.Reader c (Maybe a)) -> a -> Maybe a
+runTrans ctx f x = R.runReader (f x) ctx
+
+addStep :: a -> Eval c a ()
+addStep r = do
+  i <- gets depth
+  if i == 0 then do
+    tell [r]
   else do
-    put (Failed, i + 1)
-    return Nothing
+    return ()
 
--- Transform a rule to be recursion limited, fails to apply if
--- not terminated.
-limitrule ::  Show b => Integer
-                     -> (b -> Maybe b)
-                     -> (b -> Maybe b)
-limitrule u f x = evalState (limit u f x) istate
+eval :: (Eq a, Data a, Show a) => (Trans c a) -> a -> Eval c a a
+eval f x = do
+  ctx <- ask
+  abort <- limitReached
+  if abort then do
+    modify $ \s -> s { status = Failed }
+    return x
+  else do
+    -- transform children in bottom-up applicative order
+    incDepth
+    y <- descendM (eval f) x
+    decDepth
+    -- apply to the root
+    let z = runTrans ctx f y
+    case z of
+      Just r -> do
+        addStep x
+        incIter
+        eval f r
+      -- If in normal form then halt
+      Nothing -> do
+        return y
 
-abort :: MonadState (Status, t) m => m ()
-abort = modify $ \s -> (Failed, snd s)
-
-rewriten :: (Data a, Show a) => Integer
-                             -> (a -> Maybe a)
-                             -> a -> (a, (Status, Integer))
-rewriten n f x = runState (rewriteM (limit n f) x) istate
-
-rewritesn :: (Data a, Show a) => Integer
-                              -> [a -> Maybe a]
-                              -> a -> (a, (Status, Integer))
-rewritesn n f x = runState (rewriteM (limit n (composeM f)) x) istate
+evaluatorLoop :: (Eq a, Data a, Show a) => c -> Trans c a -> a -> (a, EvalState, [a])
+evaluatorLoop ctx f x = runRWS (eval f x) ctx defaultEvalState
