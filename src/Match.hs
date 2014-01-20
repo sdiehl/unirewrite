18d0b4afcac37c5f2df403177661403032207c75
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-01-19 21:23:59 -0600

    matching logic

--- src/Match.hs
+++ src/Match.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Match (
-  match,
   apply,
-  matches
+  matches,
+  matchesList,
+  bind,
+
+  runMatcher,
+
+  emptymatch,
+  nomatch,
+  donematch,
+
+  Matchable(..),
+  Testable(..)
 ) where
 
 import Data.Data
+import Data.Monoid
 import qualified Data.Map as Map
 
 import Control.Monad.State
@@ -12,8 +26,49 @@ import Control.Applicative
 
 import Data.Generics.Uniplate.Data
 
+-------------------------------------------------------------------------------
+-- Classes
+-------------------------------------------------------------------------------
+
+-- | Pattern match monad
 type MatchM a b = State (Subst a b) Bool
 
+
+class (Ord a, Data a) => Matchable a where
+  isPattern :: a -> Bool
+  bindPattern :: a -> a
+  match :: a -> a -> MatchM a a
+
+-- Guard conditions
+class Testable a where
+  test :: a -> Maybe Bool
+
+  testq :: a -> Bool
+  testq x = case test x of
+    (Just True) -> True
+    _           -> False
+
+
+runMatcher :: Matchable a => a -> a -> (Bool, Subst a a)
+runMatcher a b = (ms, sub)
+  where
+    (ms, sub) = runState m emptySubst
+    m = do
+      let ua = universe a
+      let ub = universe b
+      zipMatch' ua ub
+
+zipMatch' :: Matchable a => [a] -> [a] -> MatchM a a
+zipMatch' [] [] = donematch
+zipMatch' _ [] = nomatch
+zipMatch' [] _ = nomatch
+zipMatch' (x:xs) (y:ys) = match x y >> zipMatch' xs ys
+
+-------------------------------------------------------------------------------
+-- Substution
+-------------------------------------------------------------------------------
+
+-- | Substitution
 newtype Subst a b = Subst { unSubst :: Map.Map a b }
     deriving Show
 
@@ -26,41 +81,54 @@ toMap = unSubst
 emptySubst :: Subst a b
 emptySubst = fromMap Map.empty
 
+nullSubst :: Subst a b -> Bool
+nullSubst x = Map.null (toMap x)
+
 -- (s1 `compose` s2) `apply` t = s1 `apply` (s2 `apply` t).
-compose :: (Data a, Ord a) => Subst a a -> Subst a a -> Subst a a
+compose :: (Data a, Ord a, Matchable a) => Subst a a -> Subst a a -> Subst a a
 compose s1 s2 = fromMap (Map.unionWith const (apply s1 <$> toMap s2) (toMap s1))
 
-apply :: Data a => Subst a b -> a -> a
-apply b = transform rep
-  where
-    rep = undefined
-    {-rep e@(Symbol s@(Pattern _)) = Map.findWithDefault e s (toMap b)-}
-    {-rep x = x-}
+unionSubst :: Matchable a => Subst a b -> Subst a b -> Subst a b
+unionSubst s1 s2 = Subst (toMap s1 `Map.union` toMap s2)
 
+instance Matchable a => Monoid (Subst a a) where
+    mempty  = emptySubst
+    mappend = compose
+
+apply :: Matchable a => Subst a a -> a -> a
+apply b = transform go
+  where
+    go e | isPattern e = Map.findWithDefault e (bindPattern e) (toMap b)
+         | otherwise = e
+
+-- | Universal match
+emptymatch :: MatchM a b
+emptymatch = return True
+
+-- | Fail pattern
 nomatch :: MatchM a b
 nomatch = return False
 
+-- | End pattern
 donematch :: MatchM a b
 donematch = return True
 
-bind :: (Eq b, Ord a) => a -> b -> MatchM a b
+-- | Bind pattern to value
+bind :: Matchable a => a -> a -> MatchM a a
 bind p e = do
   s0 <- get
   let s = toMap s0
   -- If the bound result is identical to previously matched
   -- value then permit the non-linear pattern.
   case Map.lookup p s of
-    Just x | x == e -> return True
-           | x /= e -> return False
+    Just x | x == e    -> return True
+           | otherwise -> return False
     Nothing -> do
       put $ fromMap $ Map.insert p e s
       return True
 
-matchesList :: a -> a -> (Bool, [(k, a)])
+matchesList :: Matchable a => a -> a -> (Bool, [(a, a)])
 matchesList e p = fmap (Map.toList . toMap) $ matches e p
 
-matches :: a -> a -> (Bool, Subst b a)
+matches :: Matchable a => a -> a -> (Bool, Subst a a)
 matches pat expr = runState (match pat expr) emptySubst
-
-match :: a -> a -> MatchM b a
-match x y = undefined
