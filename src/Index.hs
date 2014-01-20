be2586f36410211743554c8b1acb60594a9353f2
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-01-20 16:19:19 -0600

    generalize indexes

--- src/Index.hs
+++ src/Index.hs
@@ -3,11 +3,9 @@ module Index where
 import Data.Data
 import Data.Generics.Uniplate.Data
 
-type Id = [Int]
-
-indexed :: Data on => on -> [(Int, on)]
+indexed :: (Enum i, Num i, Data on) => on -> [(i, on)]
 indexed tree = zip [0..] $ children tree
 
-labeled :: Data on => on -> [(Id, on)]
+labeled :: (Enum a, Data on, Num a) => on -> [([a], on)]
 labeled tree = ([], tree) : [ (x:xs, tree) | (x, subtree) <- indexed tree,
                                              (xs, tree)   <- labeled subtree ]
